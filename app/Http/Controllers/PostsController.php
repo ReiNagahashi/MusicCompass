c780142a794537841c47086ba07ab1185adc68d4
ReiRei <reirei@nagahashireinoMacBook-Pro.local>
2019-09-28 21:35:29 +0900

    updateaws5

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -92,16 +92,6 @@ class PostsController extends Controller{
             $uploadImg = $image->image = $request->file('image');
             $path = Storage::disk('s3')->putFile('/',$uploadImg,'public');
             $post->post_image = Storage::disk('s3')->url($path);
-
-                //S3clientのインスタンス生成(各項目の説明は後述)
-       $s3client = S3Client::factory([
-        'credentials' => [
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        ],
-        'region' => 'us-east-1',
-        'version' => 'latest',
-    ]);
     
     
             $post->save();
