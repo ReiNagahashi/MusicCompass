0cad8f2be41c43c96fc97dee9d10802ea2d49663
ReiRei <reirei@nagahashireinoMacBook-Pro.local>
2019-09-30 00:59:29 +0900

    forImage18

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,3 @@
-<?php
-require('vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = new Aws\S3\S3Client([
-    'version'  => 'latest',
-    'region'   => 'ap-northeast-1',
-]);
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-?>
 @extends('layouts.app')
 
 @section('content')
