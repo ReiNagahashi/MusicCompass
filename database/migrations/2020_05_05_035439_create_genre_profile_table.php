7e045889b5eb761995d8f4fb8e451c87ce37e761
Rei Nagahashi <nagahashi.bhsoft@gmail.com>
2020-05-05 10:59:00 +0700

    genreprofileTable

--- database/migrations/2020_05_05_035439_create_genre_profile_table.php
+++ database/migrations/2020_05_05_035439_create_genre_profile_table.php
@@ -1,11 +1,10 @@
-
 <?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGenreProfile extends Migration
+class CreateGenreProfileTable extends Migration
 {
     /**
      * Run the migrations.
